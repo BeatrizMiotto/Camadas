5319ba84ac79d6d681b0d64d938bd063953a5fc1
BeatrizMiotto <miottobiasforever@gmail.com>
2022-12-26 18:06:59 -0300

    inicio de conexão com database

--- console/Models/cliente.cs
+++ console/Models/cliente.cs
@@ -1,14 +1,17 @@
+using MySql.Data.MySqlClient;
+
 namespace ConsoleApp.Models;
 
 public class Cliente
 {
     public int Id {get; set;}
-    public string Nome {get; set;}
-    public string Email {get; set;}
+    public string? Nome {get; set;}
+    public string? Email {get; set;}
 
     public void Salvar()
     {
-        
+        var conexao = "Server=localhost;Database=persistencia;Uid=root;Pwd=broot;";
+        using(var connection = new MySqlConnection(conexao));
     }
 
 }
